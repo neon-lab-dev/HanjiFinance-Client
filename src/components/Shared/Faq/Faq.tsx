dbdc1e2306ce8f683cc9f5403c7b0fea6271f395
prernabadwane <prernabadwane@gmail.com>
2025-07-15 15:44:29 +0530

    added gradient to faq

--- src/components/Shared/Faq/Faq.tsx
+++ src/components/Shared/Faq/Faq.tsx
@@ -1,14 +1,16 @@
 import SectionTitle from "../../Reusable/Heading/Heading";
 import Container from "../../Reusable/Container/Container";
 import Accordion from "./Accordion";
+import { IMAGES } from "../../../assets";
 
 const Faq = () => {
   return (
-    <div className="bg-gradient-course py-[60px] font-Montserrat">
+    <div className="bg-gradient-course pt-[60px] font-Montserrat">
       <Container>
         <SectionTitle heading="Your questions matter!" />
         <Accordion />
       </Container>
+      <img src={IMAGES.coursesSectionBlur} alt="About Us" className="w-full" />
     </div>
   );
 };
