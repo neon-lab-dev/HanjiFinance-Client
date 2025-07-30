5e07bbb2b9b56ad9845b61cc5c2bb785be101320
prernabadwane <prernabadwane@gmail.com>
2025-07-30 12:03:42 +0530

    fixed get news cmpoent

--- src/components/BoardroomBanterPage/GotNews/GotNews.tsx
+++ src/components/BoardroomBanterPage/GotNews/GotNews.tsx
@@ -3,20 +3,26 @@ import Container from "../../Reusable/Container/Container";
 import SectionTitle from "../../Reusable/Heading/Heading";
 import TextInput from "../../Reusable/TextInput/TextInput";
 import Button from "../../Reusable/Button/Button";
+
 type FormData = {
-  fullName: string;
-  email: string;
+  couponCode: string;
 };
+
 const GotNews = () => {
   const {
     register,
     handleSubmit,
-    formState: { errors },
-  } = useForm<FormData>();
+    watch,
+    formState: { errors, isValid },
+  } = useForm<FormData>({ mode: "onChange" }); // validate on change
+
+  const couponCode = watch("couponCode"); // watch input value
 
   const onSubmit = (data: FormData) => {
     console.log("Form Data:", data);
   };
+
+  const isCouponValid = couponCode && !errors.couponCode; // check validity
 
   return (
     <div className="bg-gradient-good-news pt-[60px] font-Montserrat">
@@ -29,20 +35,33 @@ const GotNews = () => {
           onSubmit={handleSubmit(onSubmit)}
           className="space-y-8 flex flex-col items-center w-full"
         >
-          <div className="flex flex-col md:flex-row items-center justify-between gap-6 w-[80%] md:w-lg">
-            <TextInput
-              isRequired={false}
-              placeholder="Enter your valid coupon Code"
-              error={errors.fullName}
-              {...register("fullName", {
-                required: "Full name is required",
+          <div className="flex flex-col md:flex-row items-top justify-between gap-6 w-[80%] md:w-lg">
+            <div className="flex flex-col justify-start items-start w-full  gap-2"><TextInput
+              isRequired
+              isValidField={isCouponValid || false}
+              placeholder="Enter your valid coupon code"
+              error={errors.couponCode}
+              {...register("couponCode", {
+                required: "Coupon code is required",
+                minLength: {
+                  value: 5,
+                  message: "Coupon code must be at least 5 characters",
+                },
               })}
             />
+             {isValid && (
+                <span className="text-success-20 text-sm mt-1">
+                  Coupon Code is Correct
+                </span>
+              )}</div>
+            
+
             <Button
               type="submit"
-              label="Proceed "
-              variant="secondary"
+              label="Proceed"
+              variant={isCouponValid ? "primary" : "disabled"}
               classNames="px-4 py-[14px] h-fit"
+              disabled={!isCouponValid} // disable if not valid
             />
           </div>
         </form>
