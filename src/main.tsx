33a890b0be6eb6f54664033c867582f19f01640b
prernabadwane <prernabadwane@gmail.com>
2025-09-10 11:55:42 +0530

    completed

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import { router } from './routes/routes'
-import SmoothScroll from './components/SmoothScroll'
-import { Provider } from 'react-redux'
-import { store } from './redux/store'
-import { Toaster } from 'react-hot-toast';
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes/routes";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+import { Toaster } from "react-hot-toast";
+import AuthModal from "./components/Auth/AuthModal/AuthModal"; // 👈 import it
+import SmoothScroll from "./components/SmoothScroll";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <SmoothScroll>
-      <Provider store={store}>
-    <RouterProvider router={router} />
-     <Toaster />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+      <Toaster />
+      <AuthModal /> {/* 👈 always mounted here */}
     </Provider>
     </SmoothScroll>
   </React.StrictMode>
-)
+);
