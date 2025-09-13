c8b6f2d00ff9ce66917293a14e5155e5c7e777d4
prernabadwane <prernabadwane@gmail.com>
2025-09-13 21:04:24 +0530

    paymant page for boardroom bahter is complete

--- src/pages/Payment/Payment.tsx
+++ src/pages/Payment/Payment.tsx
@@ -1,24 +1,39 @@
+import PaymentCard from "../../components/Payment/PaymentCard/PaymentCard";
+import PaymentProductsCard from "../../components/Payment/PaymentProductsCard/PaymentProductsCard";
 import Container from "../../components/Reusable/Container/Container";
-import PaymentPage from "../../components/Shared/PaymentCard/PaymentCard";
 
 const Payment = () => {
-  const exampleItems = [
-    {
-      name: "Boardroom Banter (exclusive whatsapp space)",
-      subtitle: "Premium Service (subscription)",
-      price: 500,
-    },
-  ];
+  const data={
+    price:1000,
+    title:"boardroom banter",
+    subtitle:"connect with our group",
+    quantity:1,
+    basePrice:"1000 / month"
 
+  } 
   return (
     <div className="bg-surface-30">
-      <Container>
-        <PaymentPage
-          items={exampleItems}
-          showAutopayOption={true}
-          isAutopayAvailable={true}
-          onProceed={() => console.log("Proceed to Pay")}
-        />
+        <Container>
+        <div className="space-y-8">
+          <div className="flex justify-between items-center pb-8 border-b border-b-neutral-97">
+            <h2 className="text-[32px] text-neutral-20 md:text-neutral-35 font-bold leading-9 tracking-[-0.6px]">
+              Course Payment
+            </h2>
+          </div>
+        </div>
+
+        <div className="flex flex-col md:flex-row gap-8 md:pag-10 lg:gap-31 my-10">
+          <PaymentProductsCard item={data} />
+          <PaymentCard
+            items={[data]} 
+            gstRate={18}
+            isAutopayAvailable={true}
+            showAutopayOption={true}
+            onProceed={() => {
+              console.log("Proceed button clicked");
+            }}
+          />
+        </div>
       </Container>
       
     </div>
