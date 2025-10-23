a37adff6b3f5f57bf6d252d5b338b00b43cfffa1
rahulsd380 <rahulsd380@gmail.com>
2025-10-23 11:36:15 +0600

    Re installed files

--- src/redux/Api/baseApi.ts
+++ src/redux/Api/baseApi.ts
@@ -66,6 +66,7 @@ export const baseApi = createApi({
     "newsletter",
     "availability",
     "chatAndChill",
+    "chatAndChill",
     "boardroomBanter",
     "productOrder",
     "courseOrder",
